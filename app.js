c224e3b8a0664c58edc64720f3b88cedfb326531
BekirYldrm <bekiryildirim.2508@gmail.com>
2024-01-04 18:03:41 +0300

    project uploaded

---
+++ app.js
@@ -0,0 +1,65 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import encrypt from "mongoose-encryption";
+import { config } from "dotenv";
+
+config();
+
+
+mongoose.connect("mongodb://0.0.0.0:27017/clientDB");
+
+const userSchema = new mongoose.Schema({ email: String, password: String });
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"]});
+const userModel = mongoose.model("user", userSchema);
+
+
+const app = express();
+
+app
+    .use(bodyParser.urlencoded({ extended: true }))
+    .set("view engine", "ejs")
+
+
+    .get("/", (req, res) => {
+        res.render("home");
+    })
+
+    .get("/register", (req, res) => {
+        res.render("register")
+    })
+    .get("/login",(req,res)=>{
+        res.render("login");
+    })
+
+
+    .post("/register", (req, res) => {
+
+        const user = new userModel({
+            email: req.body.email,
+            password: req.body.password
+        })
+        user.save().then(res.render("secret")).catch(err => console.log(err))
+    })
+
+    .post("/login", (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        userModel.findOne({ email: email })
+            .then(foundUser => {
+                if (foundUser.password === password) {
+                    res.render("secret")
+                }
+                else {
+                    res.redirect("/login")
+                }
+            })
+            .catch(err => console.log(err));
+
+    })
+
+    .listen("3000", (req, res) => {
+        console.log("server running");
+    })
